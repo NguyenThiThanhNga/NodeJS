b0c780a7586ee4b4d1b6d8522003b582215636ed
nganguyen <nga.nguyen22@student.passerellesnumeriques.org>
2021-12-30 12:04:43 +0700

    part7

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -2,7 +2,7 @@ const { body,validationResult } = require('express-validator');
 var Author = require('../models/author');
 var async = require('async');
 var Book = require('../models/book');
-
+var debug = require('debug')('author');
 // Display list of all Authors.
 exports.author_list = function(req, res, next) {
 
@@ -144,9 +144,19 @@ exports.author_delete_post = function(req, res, next) {
 };
 
 
-// Display Author update form on GET.
-exports.author_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update GET');
+// Display Author update form on GET
+exports.author_update_get = function(req, res, next) {
+
+    req.sanitize('id').escape().trim();
+    Author.findById(req.params.id, function(err, author) {
+        if (err) {
+            debug('update error:' + err);
+            return next(err);
+        }
+        //On success
+        res.render('author_form', { title: 'Update Author', author: author });
+    });
+
 };
 
 // Handle Author update on POST.
